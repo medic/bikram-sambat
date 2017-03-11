52d573e5c081139f958268c509a84658b762ddc5
Alex Anderson <anderson@medicmobile.org>
2017-03-11 16:57:50 +0545

    wip

---
+++ src/index.js
@@ -0,0 +1,2 @@
+exports.str_toGreg = function(bikram) {
+};
--- test/index.js
+++ test/index.js
@@ -1,2 +1,27 @@
+var _ = require('lodash'),
+    assert = require('chai').assert;
+
+var bs = require('../src/index.js');
+
 describe('bikram-sambat', function() {
+  describe('#str_toGreg()', function() {
+    _.forIn({
+      // gregorian -> bikram
+      '1944-04-13': '2001-1-1',
+      '1944-06-19': '2001-3-6',
+    }, function(gregorian, expectedBikram) {
+
+      it('should convert ' + gregorian + ' => ' + expectedBikram, function() {
+
+        // expect
+        assert.equal(bs.str_toGreg(gregorian), expectedBikram);
+
+      });
+
+    });
+  });
+
+  describe('#daysInMonth()', function() {
+    // TODO write some tests checking the days-per-month calculations are correct for different years
+  });
 });
